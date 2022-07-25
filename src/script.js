ff52fd535d97cb935186de3c11110ce1dfc0d0fd
berscic <109431600+berscic@users.noreply.github.com>
2022-07-25 21:29:45 +0200

    showing current temp

--- src/script.js
+++ src/script.js
@@ -1,9 +1,18 @@
-let apiKey = "57c341795e71e4a5d113d1cd9d27ad8e";
+let apiKey = "e4cbb3ce899275263f94d2d4a3705a32";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
-function displayTemperature(response);
-console.log(response.data.main.temp);
-let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = response.data.main.temp;
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  console.log(response);
+}
 
 axios.get(apiUrl).then(displayTemperature);
